d4306560605c78e797dc70e2f9b112cf73edab17
axel torrico <torricohernan2@gmail.com>
2023-07-18 21:16:26 -0300

    first commit

---
+++ src/pages/app/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Home from '../home/Home';
+import MyAccount from '../myAccount/MyAccount';
+import MyOrder from '../myOrder/MyOrder';
+import MyOrders from '../myOrders/MyOrders';
+import SingIn from '../signIn/SingIn';
+import NotFound from '../notFound/NotFound';
+
+function App() {
+  return (
+    <div className='bg-cyan-950'>
+      Tailwind
+    </div>
+  )
+}
+
+export default App;
---
+++ src/pages/myOrder/MyOrder.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MyOrder() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default MyOrder;
